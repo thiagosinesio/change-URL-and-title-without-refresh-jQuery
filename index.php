60e4abd69b2275589721c1373dec9ce3292e5f76
Thiago Sinésio <thiagosinesio@gmail.com>
2013-07-03 14:41:53 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <html>
-<head>
-	<title>Change URL and title without refresh</title>
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-	<script type="text/javascript" src="js/code.js"></script>
+<head>    
+    <base href="http://<?=$_SERVER['SERVER_NAME'];?>/refresh/" />
+    <title>Change URL and title without refresh</title>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+    <script type="text/javascript" src="js/code.js"></script>
 </head>
 <body>
-	<a href="?pag=pagina1" title="Contato">Link pagina 1.1</a>
-	<a href="?pag=pagina2" title="Contato 2">Link pagina 1.2</a>
-	<a href="?pag=pagina3">Link pagina 1.3</a>
-	<?php echo '<pre>'; var_dump( $_GET ); ?>
+    <a href="?pag=pagina1" title="Pagina 1">Pagina 1</a>
+    <a href="?pag=pagina2" title="Pagina 2">Pagina 2</a>
+    <a href="?pag=pagina3" title="Pagina 3">Pagina 3</a>
+    <?php echo '<pre>'; var_dump( $_GET ); ?>
 </body>
 </html>

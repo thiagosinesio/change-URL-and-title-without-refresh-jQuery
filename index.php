eed4e3fcec52407f24c28feead4256fc170e079d
Thiago Sinésio <thiagosinesio@gmail.com>
2013-07-03 13:05:23 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,9 +5,9 @@
 <script type="text/javascript" src="js/code.js"></script>
 </head>
 <body>
-	<a href="pagina1" title="Contato">Link pagina 1.1</a>
-	<a href="pagina2" title="Contato 2">Link pagina 1.2</a>
-	<a href="pagina3">Link pagina 1.3</a>
+	<a href="?pag=pagina1" title="Contato">Link pagina 1.1</a>
+	<a href="?pag=pagina2" title="Contato 2">Link pagina 1.2</a>
+	<a href="?pag=pagina3">Link pagina 1.3</a>
 <?php echo '<pre>'; var_dump( $_GET ); ?>
 </body>
 </html>

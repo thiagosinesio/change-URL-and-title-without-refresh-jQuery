58798ed00486e38dc1c17c1ab37853dbd4dfa534
Thiago Sinésio <thiagosinesio@gmail.com>
2013-07-03 14:42:19 -0300

    Update code.js

--- js/code.js
+++ js/code.js
@@ -1,12 +1,12 @@
-var baseUrl = window.location.origin;
-
+var baseUrl = $('base').attr('href');
+//alert(baseUrl);
 $(function() {
-  $('a').click(function() {
-  	var link = $(this).attr('href');
-		var url = baseUrl+'/'+link;
-		var title = $(this).attr('title');
-		window.history.pushState(null, null, url);
-		document.title = title;
-  		return false;
- 	});
+    $('a').click(function() {
+        var link = $(this).attr('href');
+        var url = baseUrl + link;
+        var title = $(this).attr('title');
+        window.history.pushState(url, title, url);
+        document.title = title;
+        return false;
+    });
 });
